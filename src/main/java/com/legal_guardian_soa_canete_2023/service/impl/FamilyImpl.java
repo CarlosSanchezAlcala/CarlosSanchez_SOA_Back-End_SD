9824ef8c5da46ca4f6b1b9fbfe3f341495b47e3d
Carlos Sanchez <carlos.sanchez@vallegrande.edu.pe>
2023-09-20 19:50:57 -0500

    Update Family-Controller

--- src/main/java/com/legal_guardian_soa_canete_2023/service/impl/FamilyImpl.java
+++ src/main/java/com/legal_guardian_soa_canete_2023/service/impl/FamilyImpl.java
@@ -16,7 +16,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
 import static com.legal_guardian_soa_canete_2023.domain.mapper.FamilyMapper.toModel;
 
 @Slf4j
